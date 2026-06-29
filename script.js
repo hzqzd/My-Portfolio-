3cadaac12fe410715811240963a1459dc02a91f2
HAZIQ AIZAD <haziq.aizad458@gmail.com>
2026-06-29 17:32:33 +0800

    done grid and flex

--- script.js
+++ script.js
@@ -3,14 +3,19 @@ gsap.registerPlugin(Draggable);
 
 // This single line turns your HTML canvas div into a fully draggable map!
 Draggable.create("#canvas", {
-    type: "x,y",            // Allows dragging both horizontally and vertically
-    edgeResistance: 0.65,   // Adds a nice "rubber-band" bounce asset if they pull past boundaries
-    bounds: { minX: -2000, maxX: 0, minY: -2000, maxY: 0 }, // Keeps your content from getting lost in outer space
+    // Allows dragging both horizontally and vertically
+    type: "x,y",
+
+    // Adds a nice "rubber-band" bounce asset if they pull past boundaries
+    edgeResistance: 0.65,
+
+     // Keeps your content from getting lost in outer space
+    bounds: { minX: -2000, maxX: 0, minY: -2000, maxY: 0 },
     inertia: true           // Gives it that smooth momentum/throwing glide effect
 });
 
 // to position the starting viewport when loading the page
 gsap.set("#canvas", { 
-    x: -1200, 
-    y: -200 
+    x: -1500, 
+    y: -1500
 });
